3facf3687f7f672770270b37198d4e33af8c3382
Aakash Shigwan <vynce.founders@gmail.com>
2026-03-30 17:55:10 +0530

    7

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const requestRoutes = require('./routes/request');
 
 dotenv.config();
+const requestRoutes = require('./routes/request');
 
 const app = express();
 
