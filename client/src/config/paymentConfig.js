6054a9444dfebbd910389a5ff7f2b72b1b60af03
Aakash Shigwan <vynce.founders@gmail.com>
2026-03-31 22:36:39 +0530

    16

--- client/src/config/paymentConfig.js
+++ client/src/config/paymentConfig.js
@@ -1,11 +1,19 @@
 export const UPI_NAME = 'Akash';
 export const UPI_OPTIONS = [
-  { label: 'UPI 1', id: 'upi1@bank' },
+  { label: 'UPI 1', id: '8261047808@mbk' },
   { label: 'UPI 2', id: 'upi2@bank' },
   { label: 'UPI 3', id: 'upi3@bank' },
   { label: 'UPI 4', id: 'upi4@bank' },
+  { label: 'UPI 5', id: 'upi1@bank' },
+  { label: 'UPI 6', id: 'upi2@bank' },
+  { label: 'UPI 7', id: 'upi3@bank' },
+  { label: 'UPI 8', id: 'upi4@bank' },
+  { label: 'UPI 9', id: 'upi1@bank' },
+  { label: 'UPI 10', id: 'upi2@bank' },
+  { label: 'UPI 11', id: 'upi3@bank' },
+  { label: 'UPI 12', id: 'upi4@bank' },
 ];
 
 // Replace FORM_ID with the actual Google Form ID for your payment proof form.
-export const GOOGLE_FORM_ID = 'FORM_ID';
-export const GOOGLE_FORM_BASE_URL = `https://docs.google.com/forms/d/e/${GOOGLE_FORM_ID}/viewform?usp=pp_url`;
+export const GOOGLE_FORM_ID = '1FAIpQLScDJN0vNWIEvfDKUULxY_KMBJIJSjf7crYyXMDKVnYYW5NRNg';
+export const GOOGLE_FORM_BASE_URL = `https://docs.google.com/forms/d/e/1FAIpQLScDJN0vNWIEvfDKUULxY_KMBJIJSjf7crYyXMDKVnYYW5NRNg/viewform?usp=dialog`;
